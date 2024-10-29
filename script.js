72ff0b03f2434565b0adac88c1fd814a37f6e3a5
Dharmendra59 <singhdhas9559604443a@gmail.com>
2024-10-29 13:00:09 +0530

    change

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let speech = new SpeechSynthesisUtterance();
+
+
+let voices = [];
+let voiceSelect = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+
+    voices = window.speechSynthesis.getVoices();
+    voiceSelect.innerHTML = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+}
 document.querySelector('button').addEventListener('click', () => {
 
     let text = document.querySelector('textarea').value;
