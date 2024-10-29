84748015a20f30fc0ce9d9e1c1e9c9fb7ec0654d
Dharmendra59 <singhdhas9559604443a@gmail.com>
2024-10-29 14:14:12 +0530

    change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let speech = new SpeechSynthesisUtterance();
-
+let browse = document.querySelector("#browse");
+let textArea = document.querySelector("#text");
 
 document.querySelector('button').addEventListener('click', () => {
 
@@ -7,4 +8,25 @@ document.querySelector('button').addEventListener('click', () => {
     speech.text = text;
     window.speechSynthesis.speak(speech);
 
+})
+
+browse.addEventListener('change', (e) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const fileContent = e.target.result;
+            textArea.textContent = fileContent;
+        };
+
+        reader.onerror = function(e) {
+            console.error("Error reading file", e);
+        };
+
+        reader.readAsText(file); // Read the file as text (for CSV, TXT, etc.)
+    } else {
+        console.log("No file selected.");
+    }
 })
